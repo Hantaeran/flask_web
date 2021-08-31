040ea74281748170f5e1d41c31158974a73654c9
Your Name <you@example.com>
2021-08-31 11:33:36 +0900

    ??뭐라고저장하셨는지까먹음

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ def articles():
     list_data = Articles()
     return render_template('articles.html',data = list_data)
 
+@app.route('/detail/<ids>')
+def detail(ids):
+    list_data = Articles()
+    for data in list_data:
+        if data ['id'] == int(ids):
+            article = data
+    return render_template('article.html',article=article)
+
 if __name__ == '__main__':
     app.run( debug=True)
